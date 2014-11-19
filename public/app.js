107323ac8ec9afa3efec02b8e9194227bedf2a01
Flavio Granero <maltempe@gmail.com>
2014-11-18 23:19:03 -0200

    step2: separating ajax from dom

--- public/app.js
+++ public/app.js
@@ -1,17 +1,21 @@
 // app.js
+function addComment(comment, callback) {
+  $.ajax({
+    url: '/comments',
+    type: 'POST',
+    dataType: 'json',
+    data: { text: comment },
+    success: callback
+  });
+};
+
 $(document).ready(function() {
   $('#comment-form').on('submit', function(e) {
     e.preventDefault();
-    var $this = $(this);
-    $.ajax({
-      url: '/comments',
-      type: 'POST',
-      dataType: 'json',
-      data: { text: $this.find('textarea').val() },
-      success: function(data) {
-        $('#comment-list').append('<li>' + data.text + '</li>');
-        $this.find('textarea').val('');
-      }
+    var $textarea = $(this).find('textarea');
+    addComment($textarea.val(), function(data) {
+      $('#comment-list').append('<li>' + data.text + '</li>');
+      $textarea.val('');
     });
   });
 });
